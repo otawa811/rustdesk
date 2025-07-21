9725b32c1b948050e40efb772edecc00854f7dc7
otawa811 <dnfl08110317@qq.com>
2025-07-21 19:11:45 +0800

    3

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -600,15 +600,16 @@ class _DesktopHomePageState extends State<DesktopHomePage>
           margin: EdgeInsets.fromLTRB(
               0, marginTop, 0, bind.isIncomingOnly() ? marginTop : 0),
           child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [
-                  Color.fromARGB(255, 226, 66, 188),
-                  Color.fromARGB(255, 244, 114, 124),
-                ],
-              )),
+              // 【已注释：移除粉色渐变背景】
+              // decoration: BoxDecoration(
+              //     gradient: LinearGradient(
+              //   begin: Alignment.centerLeft,
+              //   end: Alignment.centerRight,
+              //   colors: [
+              //     Color.fromARGB(255, 226, 66, 188),
+              //     Color.fromARGB(255, 244, 114, 124),
+              //   ],
+              // )),
               padding: EdgeInsets.all(20),
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,

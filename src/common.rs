1349e450be59b52c14b83de740b8fe9936ac64f6
otawa811 <dnfl08110317@qq.com>
2025-07-21 18:22:28 +0800

    修改

--- src/common.rs
+++ src/common.rs
@@ -1004,12 +1004,12 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://192.168.2.1".to_owned()
 }
 
 #[inline]
 pub fn is_public(url: &str) -> bool {
-    url.contains("rustdesk.com")
+    url.contains("192.168.2.1")
 }
 
 pub fn get_udp_punch_enabled() -> bool {
